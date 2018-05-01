29c80abe3a327156f9a8e045b07caaddf072796c
Samuel Walker <31997383+samuel-walker@users.noreply.github.com>
2018-05-01 16:50:18 -0700

    Pyhton script working to download KC content

--- generateChapters.py
+++ generateChapters.py
@@ -3,9 +3,11 @@
 # import required modules
 import os # lib for operating system operations
 import csv # lib for reading csv
-import requests  # lib for reading url as file
+# import requests  # lib for reading url as file
 import re # regex
-import wget
+import wget # for downloading files
+import bs4 # beautifulsoup4 for scraping KC
+import subprocess # for calling pandoc from cmd
 
 # function to split camel case string to list
 def camel_case_split(identifier):
@@ -22,8 +24,30 @@ with open('chapters.csv', 'r') as csvfile: # open csv
     next(csvfile, None) # skip header
     data = csv.reader(csvfile, delimiter=',') # define csvreader
     for row in data: # iterate on urls to generate chapters
-        if row[0] == "": # don't worry about sections not in other books
+        if not (row[0] + row[9]): # don't worry about sections not in KC
             pass
+        elif row[9]: # download process for KC articles
+            if not os.path.isfile(row[8]): # check if file already exists
+                read = row[8][:-3] + "_read.html"
+                write = row[8][:-3] + ".html"
+                wget.download(row[9], read) # if not, download it from url
+            with open(read, "r", encoding="utf8") as html_read: #
+                html_write = open(write, "w", encoding="utf8")
+                # extract div
+                soup = bs4.BeautifulSoup(html_read, "html.parser")
+                div = soup.find("div", {"class": "kbentry post row-fluid"})
+                content = div.prettify().replace("=\"/storage/attachments/", "=\"https://knowledge.safe.com/storage/attachments/")
+                html_write.write(str(content)) # .encode('utf-8')))
+            # close files
+            html_write.close()
+            os.remove(read)
+            html_read.close()
+            # use pandoc to convert to md
+            subprocess.run("pandoc " + write + " --extract-media=images --strip-comments -f html-native_divs-native_spans -t markdown -o "
+                                    + write[:-5] + ".md",
+                                    check=True,
+                                    shell=True)
+            os.remove(write)
         else:
             # check if directory exists, make it if not
             if not os.path.exists(row[5]):
