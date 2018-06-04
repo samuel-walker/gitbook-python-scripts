da3323b88ba5b63d43ae2f46e6a8bf487a53773c
Samuel Walker <31997383+samuel-walker@users.noreply.github.com>
2018-06-04 13:07:54 -0700

    Script updates
    Working edits.py; comment clarification in generateChapters.py.

--- edits.py
+++ edits.py
@@ -1,38 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Edits existing content by inserting or replacing
 # an include to edit or add content. Run after downloading warehousing
 # generateChapters.py.
 
-import csv
 import os
+import time
+import json
 
-# Function to read edits from CSV and write to chapter md file
-def editBook(edit_file = "edits.csv"):
-    with open(edit_file) as csvfile: # open csv
-        next(csvfile, None) # skip header
-        data = csv.reader(csvfile, delimiter=',') # define csvreader
-        for row in data: # iterate on urls to generate chapters
-            section = row[0]
-            type = row[1]
-            line_num = int(row[2])
-            include = row[3]
-            with open(section, "r", encoding="utf8") as infile: # open chapter to write
-                with open(section + "_write.md", "w", encoding="utf8") as outfile: # open chapter to write
-                    for index, line in enumerate(infile, start=1):
+# Function to read edits from JSON and make them to Markdown files
+def editBook(edit_file = "edits.json"):
+    # Open json
+    with open('edits.json', encoding='utf-8') as data_file:
+        # Load json into data
+        data = json.loads(data_file.read())
+    # Iterate over sections to make edits
+    for section in data:
+        # Save attributes of edit as variable
+        attr = data[section]
+        # Open chapter to read
+        with open(section, "r", encoding="utf8") as infile:
+            # Open chapter to write
+            with open(section + "_write.md", "w", encoding="utf8") as outfile:
+                print("Editing " + section)
+                # Iterate over md reading file
+                for index, line in enumerate(infile, start=1):
+                    # Iterate over edits by section
+                    for edit in attr:
+                        # Define edit variables
+                        line_num = edit["line_num"]
+                        type = edit["type"]
+                        content = edit["content"]
+                        # Make edits if on an edited line
                         if index == line_num:
-                            if type == "include": # includes just add an include
+                            # Insert an include
+                            if type == "insert":
                                 print("Add line " + str(index))
-                                line = "\n{% include \"../includes/" + include + ".md\" %} \n\n"
-                            elif type == "replace": # replace replace lines
-                                if include == "":
-                                    print("Delete line")
-                                    line = "\n"
-                                else:
-                                    print("Replace line " + str(index))
-                                    line = line.replace(line,"{% include \"../includes/" + include + ".md\" %} \n\n")
+                                line = "\n{% include \"../includes/" + content + ".md\" %} \n\n"
+                            # Replace w/ an include
+                            elif type == "replace":
+                                print("Replace line " + str(index))
+                                line = "{% include \"../includes/" + content + ".md\" %} \n\n"
+                            # Delete lines
+                            elif type == "delete":
+                                print("Delete lines")
+                                if index <= int(content.split("-")[1]):
+                                    # Delete the line
+                                    print("Delete line " + str(index))
+                                    line = ""
+                        # Otherwise don't edit the line
                         else:
                             print("Don't edit line " + str(index))
-                        outfile.write(line)
-                    infile.close()
-                    outfile.close()
-            os.remove(section)
-            os.rename(section + "_write.md", section)
+                        # Write the line with or w/o edits from above
+                    outfile.write(line)
+        print("Finished section, clean up temp files.")
+        # Close files for reading/writing and delete temp files
+        infile.close()
+        outfile.close()
+        os.remove(section)
+        os.rename(section + "_write.md", section)
+
+# editBook()
--- generateChapters.py
+++ generateChapters.py
@@ -1,130 +1,152 @@
-#!/usr/bin/env
-# requires pandoc installed and added to path
-# likely doesn't work on Unix systems due to filepaths
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Requires pandoc installed and added to path
+# Might not work on Unix systems due to filepaths
 
-# import required modules
+# Import required modules
 import os # lib for operating system operations
 import csv # lib for reading csv
-# import requests  # lib for reading url as file
 import re # regex
 import wget # for downloading files
 import bs4 # beautifulsoup4 for scraping KC
 import subprocess # for calling pandoc from cmd
+import edits # script for making edits to downloaded content
 
-# method to split camel case string to list
+# Method to split camel case string to list
 def camel_case_split(identifier):
     matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
     return [m.group(0) for m in matches]
 
-# method to replace all items in a dic [item, replace] in text
+# Method to replace all items in a dic [item, replace] in text
 def replace_all(text, dic):
     for i, j in dic.iteritems():
         text = text.replace(i, j)
     return text
 
-# define variables
-# update bookpath if repo changes
+# Define variables
+# Update bookpath if repo changes
 bookpath = "https://raw.githubusercontent.com/safesoftware/FMETraining/"
 branch = "Desktop-Basic-2018/" # update for current branch/version
 
-# download md files and images from other books based on csv structure
-with open('chapters.csv', 'r') as csvfile: # open csv
-    next(csvfile, None) # skip header
-    data = csv.reader(csvfile, delimiter=',') # define csvreader
-    for row in data: # iterate on urls to generate chapters
-        if not (row[0] + row[9]): # don't worry about sections not in KC
+# Download md files and images from other books based on csv structure
+# Open csv
+with open('chapters.csv', 'r') as csvfile:
+    # Skip header
+    next(csvfile, None)
+    # Define csv reader
+    data = csv.reader(csvfile, delimiter=',')
+    # Iterate on urls to generate chapters
+    for row in data:
+        # For now, skip sections not in KC
+        if not (row[0] + row[9]):
             print("not")
             pass
-        elif row[9]: # download process for KC articles
-            if not os.path.isfile(row[8]): # check if file already exists
+        # Download process for KC articles
+        elif row[9]:
+            # Check if file already exists
+            if not os.path.isfile(row[8]):
                 read = row[8][:-3] + "_read.html"
                 write = row[8][:-3] + ".html"
-                wget.download(row[9], read) # if not, download it from url
-            with open(read, "r", encoding="utf8") as html_read: #
+                # If not, download it from url
+                wget.download(row[9], read)
+            # Read downloaded HTML
+            with open(read, "r", encoding="utf8") as html_read:
                 html_write = open(write, "w", encoding="utf8")
-                # extract div
+                # Extract div with content of interest
                 soup = bs4.BeautifulSoup(html_read, "html.parser")
                 div = soup.find("div", {"class": "kbentry post row-fluid"})
                 content = div.prettify().replace("=\"/storage/attachments/",
                                                  "=\"https://knowledge.safe.com/storage/attachments/")
-                html_write.write(str(content)) # .encode('utf-8')))
-            # close files, delete temp
+                html_write.write(str(content))
+            # Close files, delete temp files
             html_write.close()
             html_read.close()
             os.remove(read)
-            # use pandoc to convert to md
+            # Use pandoc to convert to md
             subprocess.run("pandoc " + write + " --extract-media=" +
                            row[8].rsplit('/', 1)[-2]
                            + "/Images --strip-comments -f html-native_divs-native_spans -t markdown -o "
                            + write[:-5] + "_read.md",
                            check=True,
                            shell=True)
-            # open md files
+            # Open md files
             read = row[8][:-3] + "_read.md"
             write = row[8][:-3] + ".md"
             with open(read, "r", encoding="utf8") as md_read: #
                 md_write = open(write, "w", encoding="utf8")
-                # extract div
+                # Extract div
                 for line in md_read:
-                    # md replacements
-                    # w_dic = {"![](" + row[5]:"",
-                    #          "###":"",
-                    #          "Article created with FME Desktop 2018.0":"",
-                    #          "[thub.nodes.view.add-new-comment](#)":""
-                    #          }
-                    # line = replace_all(line, w_dic)
-                    # text = replace_all(line, c_dic)
+                    # MD file replacements (should improve this)
                     line = line.replace("![](" + row[5] + "/", "![](")
                     line = line.replace("###", "")
                     line = line.replace("Article created with FME Desktop 2018.0", "")
                     line = line.replace("[thub.nodes.view.add-new-comment](#)", "")
                     line = line.replace("***Note:** this video was created with FME version 2016.0. Some of the steps might be slightly different, but the overall process is the same. The instructions below are for 2018.0.*", "")
-                    md_write.write(line) # .encode('utf-8')))
-            # close files and delete them
+                    md_write.write(line)
+            # Close files and delete them
             md_write.close()
             os.remove(read)
             md_read.close()
             os.remove(row[8][:-3] + ".html")
             print("KC")
+        # Non-KC chapter download process
         else:
-            # check if directory exists, make it if not
+            # Check if directory exists, make it if not
             if not os.path.exists(row[5]):
                 os.makedirs(row[5])
             if not os.path.exists(row[5] + "/Images"):
                 os.makedirs(row[5] + "/Images")
-            # download md file
-            url = bookpath + branch + row[7] # form url
+            # Download MD file
+            # Form url
+            url = bookpath + branch + row[7]
             print("MD url is " + url)
-            if not os.path.isfile(row[8]): # check if file already exists
+            # Check if file already exists
+            if not os.path.isfile(row[8]):
                 print("Downloading " + url)
-                wget.download(url, row[8]) # if not, download it
-            # download images
+                # If not, download it
+                wget.download(url, row[8])
+            # Download images
             with open(row[8], encoding="utf8") as md_read:
-                for line in md_read: # iterate on md file lines
-                    if "](./" in line: # look for images by line
-                        # grab image filename
+                # Iterate on md file lines to look for image links
+                for line in md_read:
+                    # Look for images by line
+                    if "](./" in line:
+                        # Grab image filename
                         imgsplit = line.strip()[6:-1].rsplit('/', 1)[-1]
-                        # form url
+                        # Form url
                         url = bookpath + branch + row[0] + "/Images/" + imgsplit
                         print("Image url is " + url)
-                        # check if file already exists
+                        # Check if file already exists
                         if not os.path.exists(row[5] + "/Images/" + imgsplit):
-                            # if not, download it
+                            # If not, download it
                             print("Downloading image " + url)
                             wget.download(url, row[5] + "/Images/" + imgsplit)
-            print("End normal loop")
-            print(url)
 
-# generate summary.md to create book structure
-summary = open("SUMMARY.md","w") # open summary.md to write
-summary.write("# Summary \n\n") # write summary title
-with open('chapters.csv', 'r') as csvfile: # open csv
-    next(csvfile, None) # skip header
-    data = csv.reader(csvfile, delimiter=',') # define csvreader
-    for row in data: # read rows in csv
-        # camel case to proper title
-        title = ' '.join(camel_case_split(row[6]))
-        # write link in summary with proper indentation
-        summary.write(2*int(row[3])*" " + "* [" + title[5:-3] + "](" + row[8] + ")\n")
+# Generate summary.md to create book structure; could probably combine with above
+# Open SUMMARY.md to write
+summary = open("SUMMARY.md","w")
+# Write summary title
+summary.write("# Summary \n\n")
+# Open CSV
+with open('chapters.csv', 'r') as csvfile:
+    # Skip header
+    next(csvfile, None)
+    # Define csvreader
+    data = csv.reader(csvfile, delimiter=',')
+    # Read rows in csv
+    for row in data:
+        # If an include not a section, don't add to summary
+        if row[2] == "":
+            pass
+        else:
+            # Camel case to proper title
+            title = ' '.join(camel_case_split(row[6]))
+            # Write link in summary with proper indentation
+            summary.write(2*int(row[3])*" " + "* [" + title[5:-3] + "](" + row[8] + ")\n")
 
-summary.close() # close summary.md
+# Close summary.md
+summary.close()
+
+# Run edit book function from edits.py, which makes edits according to
+# edits.json
+edits.editBook()
